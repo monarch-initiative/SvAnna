d8d38e7d4fe8d517c40c1838127b1a1678cd9999
Daniel Danis <daniel.danis@jax.org>
2020-10-30 12:12:15 +0100

    Set up the prioritization story, prepare test data

--- src/test/java/org/jax/svann/priority/PrototypeSvPrioritizerTest.java
+++ src/test/java/org/jax/svann/priority/PrototypeSvPrioritizerTest.java
@@ -17,13 +17,21 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- *
+ * Let's assume we're running prioritization for a patient with a causal SV in GCK gene (glucokinase -- hexokinase
+ * specific pancreatic beta-cells). Variants in GCK lead to MODY2 - a mild form of non-insulin dependent diabetes mellitus
+ * manifesting with a mild fasting hyperglycemia.
+ * <p>
+ * Thus, the patient might have terms such as HP:0003074 (Hyperglycemia), and HP:0025502 (Overweight).
  */
 public class PrototypeSvPrioritizerTest extends TestBase {
 
-    private static final Set<TermId> PATIENT_TERM_IDS = Set.of(
-            TermId.of("HP:0003128"), // Lactic acidosis
-            TermId.of("HP:0001266") // Choreoathetosis
+    /**
+     * Assuming the patient has variant in GCK, hyperglycemia and overweight, these are the sensible relevant enhancer
+     * top level terms.
+     */
+    private static final Set<TermId> RELEVANT_ENHANCER_TOP_LEVEL_TERMS = Set.of(
+            TermId.of("HP:0001939"), // Abnormality of metabolism/homeostasis, based on Hyperglycemia
+            TermId.of("HP:HP:0001507") // Growth abnormality, based on Overweight
     );
 
     // TODO(PNR): 10/29/20 complete
@@ -37,28 +45,31 @@ public class PrototypeSvPrioritizerTest extends TestBase {
     }
 
     /**
-     * Prepare an interval array map with a single enhancer interval array for chromosome 9, where SURF1 and SURF2
-     * are located.
-     *
-     * The interval array contains a single enhancer spanning the region chr9:133,356,500-133,356,550, which is
-     * a region upstream from SURF1 5'UTR, thus a realistic region.
-     *
-     * The enhancer has fictional tau=0.8 and TermId `HP:0001939` (Abnormality of metabolism/homeostasis)
-     *
+     * Prepare an interval array map with a enhancers for <em>SURF1</em> and <em>SURF2</em> (chr9) and <em>GCK</em>
+     * (chr7).
+     * <p>
+     * The interval array contains a single enhancer per gene:
+     * <ul>
+     *     <li><em>SURF[12]</em> - region chr9:133,356,500-133,356,550</li>
+     *     <li><em>GCK</em> - region chr7:44,190,001-44,190,050</li>
+     * <p>
+     * Both enhancers have fictional tau=0.8 and TermId `HP:0001939` (Abnormality of metabolism/homeostasis)
      */
     private static Map<Integer, IntervalArray<Enhancer>> makeEnhancerMap() {
         Contig chr9 = GENOME_ASSEMBLY.getContigByName("9").orElseThrow();
+        Contig chr7 = GENOME_ASSEMBLY.getContigByName("7").orElseThrow();
 
-        Enhancer enhancer = new Enhancer(chr9, 133_356_500, 133_356_550, .8, TermId.of("HP:0001939"));
+        Enhancer surf1Enhancer = new Enhancer(chr9, 133_356_500, 133_356_550, .8, TermId.of("HP:0001939"));
+        Enhancer gckEnhancer = new Enhancer(chr7, 44_190_001, 44_190_050, .8, TermId.of("HP:0001939"));
 
-        List<Enhancer> enhancers = List.of(enhancer);
-        IntervalArray<Enhancer> eArray = new IntervalArray<>(enhancers, new EnhancerEndExtractor());
-        return Map.of(9, eArray);
+        IntervalArray<Enhancer> chr7Array = new IntervalArray<>(List.of(gckEnhancer), new EnhancerEndExtractor());
+        IntervalArray<Enhancer> chr9Array = new IntervalArray<>(List.of(surf1Enhancer), new EnhancerEndExtractor());
+        return Map.of(7, chr7Array, 9, chr9Array);
     }
 
     @BeforeEach
     public void setUp() {
-        prioritizer = new PrototypeSvPrioritizer(GENOME_ASSEMBLY, PATIENT_TERM_IDS, ENHANCER_MAP, DISEASE_MAP, JANNOVAR_DATA);
+        prioritizer = new PrototypeSvPrioritizer(GENOME_ASSEMBLY, RELEVANT_ENHANCER_TOP_LEVEL_TERMS, ENHANCER_MAP, DISEASE_MAP, JANNOVAR_DATA);
     }
 
     @Test
@@ -68,4 +79,17 @@ public class PrototypeSvPrioritizerTest extends TestBase {
         System.err.println(result);
     }
 
+    @Test
+    public void prioritize_upstreamDeletion_GCK_inEnhancer() {
+        SequenceRearrangement se = TestVariants.deletionGCKUpstreamIntergenic_affectingEnhancer();
+        PrioritizedSv result = prioritizer.prioritize(se);
+        System.err.println(result);
+    }
+
+    @Test
+    public void prioritize_upstreamDeletion_GCK_notInEnhancer() {
+        SequenceRearrangement se = TestVariants.deletionGCKUpstreamIntergenic_NotAffectingEnhancer();
+        PrioritizedSv result = prioritizer.prioritize(se);
+        System.err.println(result);
+    }
 }

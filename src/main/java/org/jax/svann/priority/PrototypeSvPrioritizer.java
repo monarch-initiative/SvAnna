d9927051c7dcc22cf04d68882c913081f4ff5c03
Daniel Danis <daniel.danis@jax.org>
2020-10-30 12:12:15 +0100

    Set up the prioritization story, prepare test data

--- src/main/java/org/jax/svann/priority/PrototypeSvPrioritizer.java
+++ src/main/java/org/jax/svann/priority/PrototypeSvPrioritizer.java
@@ -1,11 +1,8 @@
 package org.jax.svann.priority;
 
 import de.charite.compbio.jannovar.data.JannovarData;
-import de.charite.compbio.jannovar.data.JannovarDataSerializer;
 import de.charite.compbio.jannovar.data.ReferenceDictionary;
-import de.charite.compbio.jannovar.data.SerializationException;
 import de.charite.compbio.jannovar.impl.intervals.IntervalArray;
-import org.jax.svann.except.SvAnnRuntimeException;
 import org.jax.svann.genomicreg.Enhancer;
 import org.jax.svann.hpo.HpoDiseaseSummary;
 import org.jax.svann.overlap.EnhancerOverlapper;
@@ -25,28 +22,31 @@ import java.util.stream.Collectors;
  */
 public class PrototypeSvPrioritizer implements SvPrioritizer {
 
+    private static final Set<HpoDiseaseSummary> EMPTYSET = Set.of();
+
+    /**
+     * Genome assembly in use.
+     */
     private final GenomeAssembly assembly;
-    /** Collection of top level HPO ids that are equal to or ancestors of terms used to annotate the clinical
+
+    /**
+     * Collection of top level HPO ids that are equal to or ancestors of terms used to annotate the clinical
      * features of the individual being investigated.
      */
     private final Set<TermId> relevantHpoIdsForEnhancers;
 
-
-    /** Key: A chromosome id; value: a Jannovar Interval array for searching for overlapping enhancers. */
+    /**
+     * Key: A chromosome id; value: a Jannovar Interval array for searching for overlapping enhancers.
+     */
     private final Map<Integer, IntervalArray<Enhancer>> chromosomeToEnhancerIntervalArrayMap;
-
     private final Overlapper overlapper;
 
     private final EnhancerOverlapper enhancerOverlapper;
-
 
     /**
      * Reference dictionary (part of {@link JannovarData}).
      */
     private final ReferenceDictionary rd;
-
-    private static final Set<HpoDiseaseSummary> EMPTYSET = Set.of();
-
 
     /**
      * This map is initialized to contain only those gene ids that are associated with diseases that
@@ -55,10 +55,10 @@ public class PrototypeSvPrioritizer implements SvPrioritizer {
     private final Map<TermId, Set<HpoDiseaseSummary>> relevantGeneIdToAssociatedDiseaseMap;
 
     public PrototypeSvPrioritizer(GenomeAssembly assembly,
-                           Set<TermId> relevantHposForEnhancers,
-                           Map<Integer, IntervalArray<Enhancer>> enhancerMap,
-                           Map<TermId, Set<HpoDiseaseSummary>> gene2diseaseMap,
-                           JannovarData jannovarData) {
+                                  Set<TermId> relevantHposForEnhancers,
+                                  Map<Integer, IntervalArray<Enhancer>> enhancerMap,
+                                  Map<TermId, Set<HpoDiseaseSummary>> gene2diseaseMap,
+                                  JannovarData jannovarData) {
         this.assembly = assembly;
         this.relevantHpoIdsForEnhancers = relevantHposForEnhancers;
         this.chromosomeToEnhancerIntervalArrayMap = enhancerMap;
@@ -71,6 +71,40 @@ public class PrototypeSvPrioritizer implements SvPrioritizer {
 
     @Override
     public PrioritizedSv prioritize(SequenceRearrangement rearrangement) {
+        /*
+        For each case we need to figure out the affected transcripts/genes.
+        Then, we figure out the impact on the sequence (high/medium/low) and phenotype relevance.
+
+        Sequence impact category:
+        - use overlapper to get the overlapping transcripts
+        - if overlap type is intergenic:
+          - if variant disrupts an enhancer with tissue relevance -> medium sequence impact
+            - else low
+        - if overlap type is single/multiple/entire tx affected -> high sequence impact
+        report the most severe sequence impact category
+
+        Phenotypic relevance:
+        - the affected gene is among the relevant genes -> relevant
+        - else not
+
+        Note that the rules for getting "overlap" depends on SvType
+         */
+
+        switch (rearrangement.getType()) {
+            case DELETION:
+//                return prioritizeDeletion(rearrangement);
+                break;
+            case INVERSION:
+//                return prioritizeInversion(rearrangement);
+                break;
+            case INSERTION:
+//                return prioritizeInsertion(rearrangement);
+                break;
+            case TRANSLOCATION:
+//                return prioritizeTranslocation(rearrangement);
+                break;
+        }
+
         List<Overlap> overlaps = overlapper.getOverlapList(rearrangement);
         // Here we want to do something fancy to see if an SV interrupts enhancer-gene interactions, e.g., Inversion,
         // but for now let's just see if the SV directly affects and enhancer
@@ -94,13 +128,20 @@ public class PrototypeSvPrioritizer implements SvPrioritizer {
 
     }
 
+    private PrioritizedSv prioritizeDeletion(SequenceRearrangement rearrangement) {
 
-    private static JannovarData readJannovarData(String jannovarDataPath)  {
-        try {
-            JannovarData jdata = new JannovarDataSerializer(jannovarDataPath).load();
-            return jdata;
-        } catch (SerializationException e) {
-            throw new SvAnnRuntimeException("Could not input Jannovar transcript data: " + e.getMessage());
-        }
+        return null;
+    }
+
+    private PrioritizedSv prioritizeInversion(SequenceRearrangement rearrangement) {
+        return null;
+    }
+
+    private PrioritizedSv prioritizeInsertion(SequenceRearrangement rearrangement) {
+        return null;
+    }
+
+    private PrioritizedSv prioritizeTranslocation(SequenceRearrangement rearrangement) {
+        return null;
     }
 }

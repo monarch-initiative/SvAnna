5d8c276ca5b0ea91050b75a018b419a3911cd254
Peter Robinson <peter.robinson@jax.org>
2020-11-01 09:53:22 -0500

    split prioritization into sequence and phenotype steps

---
+++ src/main/java/org/jax/svann/priority/PhenotypeSvPrioritizer.java
@@ -0,0 +1,10 @@
+package org.jax.svann.priority;
+
+import org.jax.svann.reference.SequenceRearrangement;
+
+public class PhenotypeSvPrioritizer implements SvPrioritizer {
+    @Override
+    public SvPriority prioritize(SequenceRearrangement rearrangement) {
+        return null;
+    }
+}

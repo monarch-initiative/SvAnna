082fc568284d582fd86350a07a5b64f531490a10
Peter Robinson <peter.robinson@jax.org>
2020-11-01 19:36:04 -0500

    display Sv type

--- src/main/java/org/jax/svann/viz/HtmlVisualizable.java
+++ src/main/java/org/jax/svann/viz/HtmlVisualizable.java
@@ -1,6 +1,7 @@
 package org.jax.svann.viz;
 
 import org.jax.svann.except.SvAnnRuntimeException;
+import org.jax.svann.hpo.HpoDiseaseSummary;
 import org.jax.svann.priority.SvImpact;
 import org.jax.svann.priority.SvPriority;
 import org.jax.svann.reference.Adjacency;
@@ -9,18 +10,13 @@ import org.jax.svann.reference.SequenceRearrangement;
 import org.jax.svann.reference.SvType;
 import org.jax.svann.reference.genome.Contig;
 
-import java.util.List;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 
 public class HtmlVisualizable implements  Visualizable {
 
     final private SvType svType;
 
     final private SvImpact svImpact;
-
-    final private Map<String, String> locationMap;
 
     final private SvPriority svPriority;
 
@@ -38,10 +34,9 @@ public class HtmlVisualizable implements  Visualizable {
         this.rearrangement = svPriority.getRearrangement();
         this.svType = this.rearrangement.getType();
         this.svPriority = svPriority;
-        this.locationMap =  initializeLocationStrings();
     }
 
-    private String getDeletionLocationString(SequenceRearrangement rearrangement) {
+    private HtmlLocation getDeletionLocation(SequenceRearrangement rearrangement) {
         List<Adjacency> adjacencies = rearrangement.getAdjacencies();
         if (adjacencies.size() != 1) {
             throw new SvAnnRuntimeException("Malformed deletion adjacency list with size " + adjacencies.size());
@@ -50,28 +45,11 @@ public class HtmlVisualizable implements  Visualizable {
         Breakend left = deletion.getLeft();
         Breakend right = deletion.getRight();
         Contig chrom = left.getContig();
-        int id = chrom.getId();
         int begin = left.getBegin();
         int end = right.getEnd();
-        return String.format("chr%s:%d-%d", id, begin,end);
+        return new HtmlLocation(chrom, begin,end);
     }
 
-    /**
-     * Create strings such as chr3:123-432 for the structural variant. For some kinds of SV, we will
-     * want to return multiple rows.
-     * @return a map of key value pairs representing the locations
-     */
-    private Map<String, String> initializeLocationStrings() {
-        SortedMap<String, String> sortedMap = new TreeMap<>();
-        if (this.rearrangement.getType() == SvType.DELETION) {
-            String loc = getDeletionLocationString(this.rearrangement);
-            sortedMap.put("location", loc);
-        } else {
-            sortedMap.put("todo #1", "A");
-            sortedMap.put("todo #2", "B");
-        }
-        return sortedMap;
-    }
 
     @Override
     public SequenceRearrangement getRearrangement() {
@@ -83,10 +61,6 @@ public class HtmlVisualizable implements  Visualizable {
         return svType.toString();
     }
 
-    @Override
-    public Map<String, String> getLocationStrings() {
-        return this.locationMap;
-    }
 
     @Override
     public String getImpact() {
@@ -96,5 +70,26 @@ public class HtmlVisualizable implements  Visualizable {
     @Override
     public boolean hasPhenotypicRelevance() {
         return this.svPriority.hasPhenotypicRelevance();
+    }
+
+    public List<HpoDiseaseSummary> getDiseaseSummaries() { return this.svPriority.getDiseases(); }
+
+    /**
+     * Return strings for display of the format chr3:123-456
+     * @return
+     */
+    @Override
+    public List<HtmlLocation> getLocations() {
+        List<HtmlLocation> locs = new ArrayList<>();
+        if (rearrangement.getType() == SvType.DELETION) {
+            locs.add(getDeletionLocation(rearrangement));
+        } else  if (rearrangement.getType() == SvType.INSERTION) {
+            int c = 42;
+            int y = 32;
+        }
+
+
+
+        return locs;
     }
 }

bd4ba1c30c7524faba6144b42cfa86633002f649
Daniel Danis <daniel.danis@jax.org>
2020-11-04 23:27:16 +0100

    Deprecate Position tests

--- src/test/java/org/jax/svann/reference/PositionTest.java
+++ src/test/java/org/jax/svann/reference/PositionTest.java
@@ -1,14 +1,17 @@
 package org.jax.svann.reference;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@Deprecated
+@Disabled
 public class PositionTest {
 
     private Position precise, imprecise;

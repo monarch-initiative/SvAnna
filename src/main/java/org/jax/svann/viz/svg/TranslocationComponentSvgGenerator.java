53206a444dba7046b23140f3c6a29309936e2046
Daniel Danis <daniel.danis@jax.org>
2020-11-09 22:04:44 +0100

    Use domain classes in TranslocationComponentSvgGenerator

--- src/main/java/org/jax/svann/viz/svg/TranslocationComponentSvgGenerator.java
+++ src/main/java/org/jax/svann/viz/svg/TranslocationComponentSvgGenerator.java
@@ -1,9 +1,9 @@
 package org.jax.svann.viz.svg;
 
-import de.charite.compbio.jannovar.reference.TranscriptModel;
 import org.jax.svann.genomicreg.Enhancer;
 import org.jax.svann.reference.CoordinatePair;
 import org.jax.svann.reference.SvType;
+import org.jax.svann.reference.transcripts.SvAnnTxModel;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -24,7 +24,7 @@ public class TranslocationComponentSvgGenerator extends  SvSvgGenerator {
      * @param enhancers       // * @param genomeInterval
      * @param coordinatePairs
      */
-    public TranslocationComponentSvgGenerator(List<TranscriptModel> transcripts, List<Enhancer> enhancers, List<CoordinatePair> coordinatePairs) {
+    public TranslocationComponentSvgGenerator(List<SvAnnTxModel> transcripts, List<Enhancer> enhancers, List<CoordinatePair> coordinatePairs) {
         super(SvType.TRANSLOCATION, transcripts, enhancers, coordinatePairs);
     }
 
--- src/test/java/org/jax/svann/viz/svg/TranslocationSvGeneratorTest.java
+++ src/test/java/org/jax/svann/viz/svg/TranslocationSvGeneratorTest.java
@@ -12,24 +12,20 @@ package org.jax.svann.viz.svg;
 
 import de.charite.compbio.jannovar.data.ReferenceDictionary;
 import de.charite.compbio.jannovar.impl.intervals.IntervalArray;
-import de.charite.compbio.jannovar.reference.GenomeInterval;
-import de.charite.compbio.jannovar.reference.PositionType;
-import de.charite.compbio.jannovar.reference.Strand;
 import de.charite.compbio.jannovar.reference.TranscriptModel;
 import org.jax.svann.TestBase;
 import org.jax.svann.genomicreg.Enhancer;
-import org.jax.svann.genomicreg.TestCoordinatePair;
-import org.jax.svann.genomicreg.TestGenomicPosition;
-import org.jax.svann.hpo.GeneWithId;
 import org.jax.svann.hpo.HpoDiseaseSummary;
 import org.jax.svann.overlap.EnhancerOverlapper;
 import org.jax.svann.overlap.Overlap;
 import org.jax.svann.overlap.Overlapper;
+import org.jax.svann.overlap.SvAnnOverlapper;
 import org.jax.svann.priority.PrototypeSvPrioritizer;
 import org.jax.svann.priority.SvPrioritizer;
 import org.jax.svann.priority.SvPriority;
-import org.jax.svann.reference.*;
-import org.jax.svann.reference.genome.Contig;
+import org.jax.svann.reference.CoordinatePair;
+import org.jax.svann.reference.SequenceRearrangement;
+import org.jax.svann.reference.transcripts.SvAnnTxModel;
 import org.jax.svann.viz.HtmlVisualizable;
 import org.jax.svann.viz.HtmlVisualizer;
 import org.junit.jupiter.api.Test;
@@ -38,23 +34,26 @@ import org.monarchinitiative.phenol.ontology.data.TermId;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.jax.svann.parse.TestVariants.translocationWhereOneCdsIsDisruptedAndTheOtherIsNot;
+import static org.jax.svann.parse.TestVariants.Translocations;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
-public class TranslocationSvGeneratorTest extends TestBase{
+public class TranslocationSvGeneratorTest extends TestBase {
     private static final Collection<TranscriptModel> transcripts = JANNOVAR_DATA.getTmByAccession().values();
     private static final ReferenceDictionary rd = JANNOVAR_DATA.getRefDict();
     private static final List<Enhancer> enhancers = List.of();
     private static final Set<TermId> enhancerRelevantAncestors = Set.of();
-    private static final SequenceRearrangement translocation = translocationWhereOneCdsIsDisruptedAndTheOtherIsNot();
+    private static final SequenceRearrangement translocation = Translocations.translocationWhereOneCdsIsDisruptedAndTheOtherIsNot();
     private static final Map<Integer, IntervalArray<Enhancer>> enhancerMap = Map.of();
     private static final Set<TermId> patientTerms = Set.of();
-    private static final Overlapper overlapper = new Overlapper(JANNOVAR_DATA);
-    private static final EnhancerOverlapper enhancerOverlapper = new EnhancerOverlapper(JANNOVAR_DATA, enhancerMap);
+    private static final Overlapper overlapper = new SvAnnOverlapper(TX_SERVICE.getChromosomeMap());
+    private static final EnhancerOverlapper enhancerOverlapper = new EnhancerOverlapper(enhancerMap);
     private static final Map<TermId, Set<HpoDiseaseSummary>> relevantGenesAndDiseases = Map.of();
     private static final SvPrioritizer prioritizer = new PrototypeSvPrioritizer(overlapper,
             enhancerOverlapper,
@@ -76,7 +75,7 @@ public class TranslocationSvGeneratorTest extends TestBase{
     public void testWriteSvg() {
        // String svg = visualizer.getHtml(visualizable);
         List<Overlap> overlaps = overlapper.getOverlapList(translocation);
-        List<TranscriptModel> transcriptModels = overlaps.stream().map(Overlap::getTranscriptModel).collect(Collectors.toList());
+        List<SvAnnTxModel> transcriptModels = overlaps.stream().map(Overlap::getTranscriptModel).collect(Collectors.toList());
         List<Enhancer> enhancerList = List.of();
         List<CoordinatePair> cpairs = translocation.getRegions();
         SvSvgGenerator gen = new TranslocationSvgGenerator(translocation,transcriptModels, enhancerList, cpairs);

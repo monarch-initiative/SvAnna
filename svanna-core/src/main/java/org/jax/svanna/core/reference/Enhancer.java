1078b46009832cca2b4a44c2684441e13509d2b7
Daniel Danis <daniel.danis@jax.org>
2020-12-09 16:30:26 +0100

    Implement Enhancer using BaseGenomicRegion

--- svanna-core/src/main/java/org/jax/svanna/core/reference/Enhancer.java
+++ svanna-core/src/main/java/org/jax/svanna/core/reference/Enhancer.java
@@ -3,7 +3,9 @@ package org.jax.svanna.core.reference;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 import org.monarchinitiative.variant.api.*;
 
-public class Enhancer extends PreciseGenomicRegion {
+import java.util.Objects;
+
+public class Enhancer extends BaseGenomicRegion<Enhancer> {
 
     private final double tau;
     private final TermId hpoId;
@@ -12,20 +14,21 @@ public class Enhancer extends PreciseGenomicRegion {
      */
     private final String tissueLabel;
 
-    private Enhancer(GenomicRegion region, double tau, TermId hpoId, String tissueLabel) {
-        super(region);
+    private Enhancer(Contig contig, Strand strand, CoordinateSystem coordinateSystem, Position start, Position end,
+                     double tau, TermId hpoId, String tissueLabel) {
+        super(contig, strand, coordinateSystem, start, end);
         this.tau = tau;
-        this.hpoId = hpoId;
-        this.tissueLabel = tissueLabel;
+        this.hpoId = Objects.requireNonNull(hpoId);
+        this.tissueLabel = Objects.requireNonNull(tissueLabel);
     }
 
     public static Enhancer of(GenomicRegion region, double tau, TermId hpoId, String tissueLabel) {
         return of(region.contig(), region.strand(), region.coordinateSystem(), region.startPosition(), region.endPosition(), tau, hpoId, tissueLabel);
     }
+
     public static Enhancer of(Contig contig, Strand strand, CoordinateSystem coordinateSystem, Position start, Position end,
                               double tau, TermId hpoId, String tissueLabel) {
-        GenomicRegion region = PreciseGenomicRegion.of(contig, strand, coordinateSystem, start, end);
-        return new Enhancer(region, tau, hpoId, tissueLabel);
+        return new Enhancer(contig, strand, coordinateSystem, start, end, tau, hpoId, tissueLabel);
     }
 
     public double tau() {
@@ -41,22 +44,8 @@ public class Enhancer extends PreciseGenomicRegion {
     }
 
     @Override
-    public Enhancer withStrand(Strand other) {
-        return strand == other
-                ? this
-                : new Enhancer(super.withStrand(other), tau, hpoId, tissueLabel);
-    }
-
-    @Override
-    public Enhancer toOppositeStrand() {
-        return withStrand(strand.opposite());
-    }
-
-    @Override
-    public Enhancer withCoordinateSystem(CoordinateSystem other) {
-        return coordinateSystem == other
-                ? this
-                : new Enhancer(super.withCoordinateSystem(other), tau, hpoId, tissueLabel);
+    protected Enhancer newRegionInstance(Contig contig, Strand strand, CoordinateSystem coordinateSystem, Position startPosition, Position endPosition) {
+        return new Enhancer(contig, strand, coordinateSystem, startPosition, endPosition, tau, hpoId, tissueLabel) ;
     }
 
     @Override
@@ -64,22 +53,21 @@ public class Enhancer extends PreciseGenomicRegion {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
-
         Enhancer enhancer = (Enhancer) o;
-
-        if (Double.compare(enhancer.tau, tau) != 0) return false;
-        if (hpoId != null ? !hpoId.equals(enhancer.hpoId) : enhancer.hpoId != null) return false;
-        return tissueLabel != null ? tissueLabel.equals(enhancer.tissueLabel) : enhancer.tissueLabel == null;
+        return Double.compare(enhancer.tau, tau) == 0 && Objects.equals(hpoId, enhancer.hpoId) && Objects.equals(tissueLabel, enhancer.tissueLabel);
     }
 
     @Override
     public int hashCode() {
-        int result = super.hashCode();
-        long temp;
-        temp = Double.doubleToLongBits(tau);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        result = 31 * result + (hpoId != null ? hpoId.hashCode() : 0);
-        result = 31 * result + (tissueLabel != null ? tissueLabel.hashCode() : 0);
-        return result;
+        return Objects.hash(super.hashCode(), tau, hpoId, tissueLabel);
+    }
+
+    @Override
+    public String toString() {
+        return "Enhancer{" +
+                "tau=" + tau +
+                ", hpoId=" + hpoId +
+                ", tissueLabel='" + tissueLabel + '\'' +
+                "} " + super.toString();
     }
 }

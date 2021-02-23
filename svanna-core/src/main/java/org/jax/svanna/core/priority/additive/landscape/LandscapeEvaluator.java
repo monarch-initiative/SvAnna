0421b6608685821e2b077ad36dedc4024ca313db
Daniel Danis <daniel.danis@jax.org>
2021-02-23 17:15:11 -0500

    Remove landscape stuff

--- svanna-core/src/main/java/org/jax/svanna/core/priority/additive/landscape/LandscapeEvaluator.java
+++
@@ -1,9 +0,0 @@
-package org.jax.svanna.core.priority.additive.landscape;
-
-import org.monarchinitiative.svart.Variant;
-
-@Deprecated(forRemoval = true)
-public interface LandscapeEvaluator<L extends Landscape> {
-
-    double evaluateLandscape(Variant variant, L landscape);
-}

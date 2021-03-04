df5f3a01b8fd96d19bbbe4c79a5d3616ed261414
Daniel Danis <daniel.danis@jax.org>
2021-03-04 17:37:51 -0500

    Improve error handling within the task pool

--- svanna-cli/src/main/java/org/jax/svanna/cli/cmd/TaskUtils.java
+++ svanna-cli/src/main/java/org/jax/svanna/cli/cmd/TaskUtils.java
@@ -1,5 +1,9 @@
 package org.jax.svanna.cli.cmd;
 
+import org.jax.svanna.core.exception.LogUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
@@ -7,11 +11,15 @@ import java.util.concurrent.ForkJoinTask;
 
 public class TaskUtils {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskUtils.class);
+    private static final Thread.UncaughtExceptionHandler HANDLER = (thread, throwable) ->
+            LogUtils.logError(LOGGER, "Error on thread {}: {}", thread.getName(), throwable.getMessage());
+
     private TaskUtils() {
     }
 
     public static <T> T executeBlocking(Callable<T> callable, int parallelism) throws ExecutionException, InterruptedException {
-        ForkJoinPool pool = new ForkJoinPool(parallelism, SvAnnaWorkerThread::new, null, false);
+        ForkJoinPool pool = new ForkJoinPool(parallelism, SvAnnaWorkerThread::new, HANDLER, false);
         ForkJoinTask<T> task = pool.submit(callable);
         pool.shutdown();
         return task.get();

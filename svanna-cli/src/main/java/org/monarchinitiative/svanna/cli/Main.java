3ba20d23ca180a63b3a305fd8d1a72fb36b48a0c
Daniel Danis <daniel.gordon.danis@protonmail.com>
2025-11-28 13:09:17 +0100

    Next development iteration `v1.2.1-SNAPSHOT`.

--- svanna-cli/src/main/java/org/monarchinitiative/svanna/cli/Main.java
+++ svanna-cli/src/main/java/org/monarchinitiative/svanna/cli/Main.java
@@ -18,7 +18,7 @@ import static picocli.CommandLine.Help.Ansi.Style.*;
         footer = Main.FOOTER)
 public class Main implements Callable<Integer>  {
 
-    public static final String VERSION = "svanna-cli v1.2.0";
+    public static final String VERSION = "svanna-cli v1.2.1-SNAPSHOT";
 
     public static final int WIDTH = 120;
 
